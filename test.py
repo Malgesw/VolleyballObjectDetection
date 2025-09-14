4fdfad77791140e28a3a54405b3e5da7ca64fafe
Niccolo Malgeri <Malgesw@users.noreply.github.com>
2025-09-14 13:22:43 +0200

    Small refactoring; added file for saving predictions and used hyperparameters

--- test.py
+++ test.py
@@ -10,12 +10,14 @@ model_version = f"{train_epochs}_epochs_{name_preprocess}"
 with open(f"params_{name_preprocess}.txt", "r") as file:
     params = file.read()
 
+#params = "" # uncomment if runs does not have any params
+
 model = YOLO(
     f"./runs_{model_version}{params}/detect/yolo_train_{name_preprocess}{params}/weights/best.pt"
 )
 name_preprocess = "threshold_white_orange"
 preprocess("test_frames", name_preprocess, params, test=True)
-model.predict(source="test_frames" + name_preprocess + params, save=True, imgsz=640)
+results = model.predict(source="test_frames" + name_preprocess + params, save=True, imgsz=640)
 shutil.rmtree("test_frames" + name_preprocess + params)
 if os.path.exists(f"./runs_{model_version}{params}/detect/predict_test_{name_preprocess}{params}"):
     shutil.rmtree(
@@ -26,3 +28,13 @@ os.rename(
     f"./runs_{model_version}{params}/detect/predict_test_{name_preprocess}{params}",
 )
 shutil.rmtree("./runs")
+
+with open(f"predictions_{name_preprocess}{params}.txt", "w") as file:
+    for r in results:
+        boxes = r.boxes  # bounding boxes
+        for box in boxes:
+            cls_id = int(box.cls[0].item())      # class index
+            conf = float(box.conf[0].item())     # confidence
+            xyxy = box.xyxy[0].tolist()          # [x1, y1, x2, y2]
+            
+            file.write(f"class: {cls_id}, conf: {conf:.4f}, box: {xyxy}\n")
